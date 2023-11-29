4d7ea1ea16627297d0888e3008b428ba3a64ddd1
juan <104380950+juanisola@users.noreply.github.com>
2023-11-29 04:54:10 -0300

    Add files via upload

---
+++ código/servidor/lib/constants.py
@@ -0,0 +1,7 @@
+INTERNET_NAME = 'Telecentro-fef8'
+INTERNET_PASSWORD = 'super1976'
+
+SERVER_HOSTNAME = 'a8aa9c357bb34776b30489bb713fb35d.s1.eu.hivemq.cloud'
+USER = 'proyecto Shanghai'
+PASSWORD = 'Bricolaputo1'
+
